10b20e15a2905d2062e481eb6d113eadba2bbc70
Dantezy <zhangyet@gmail.com>
2026-06-12 01:19:28 +0800

    exec 2.28: Not done yet, notice that `cdr` a list will end at a nil
    Signed-off-by: Dantezy <zhangyet@gmail.com>

--- sicp/chap02/chap02_02.scm
+++ sicp/chap02/chap02_02.scm
@@ -1,3 +1,11 @@
+;; exec 2.26
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+(append x y) ; (1 2 3 4 5 6)
+(cons x y)   ; ((1 2 3) 4 5 6)
+(list x y)   ; ((1 2 3) (4 5 6))
+(car x)      ; 1
+
 ;; exec 2.27
 (define x (list (list 1 2) (list 3 4)))
 
@@ -8,3 +16,11 @@
 		      (list (deep-reverse (car x)))))))
 
 (deep-reverse x)
+
+;; exec 2.28
+(define (fringe x)
+  (cond ((not (pair? x)) (list x))
+	(else (append (fringe (car x))
+		      (fringe (cdr x))))))
+
+(fringe x)
