53c9abc78494024115f6dbffbbac04af63cb8d29
Dantezy <zhangyet@gmail.com>
2026-06-14 15:22:15 +0800

    Done 2.40

--- sicp/chap02/chap02_02.scm
+++ sicp/chap02/chap02_02.scm
@@ -226,3 +226,45 @@
 
 (define (reverse-fl sequence)
   (fold-left (lambda (x y) (cons y x)) nil sequence))
+
+;; exec 2.40
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (order? x)
+  (< (car x) (cadr x)))
+
+(define (prime? n)
+  (define (smallest-divisor n)
+    (find-divisor n 2))
+  (define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+  (define (divides? a b)
+    (= (remainder b a) 0))
+  (= n (smallest-divisor n)))
+
+(define (uniq-pairs n)
+  (flatmap (lambda (i)
+	     (map (lambda (j) (list j i))
+		  (enumerate-interval 1 (- i 1))))
+	   (enumerate-interval 1 n)))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pair? p)
+  (prime? (+ (car p) (cadr p))))
+
+(define (prime-sum-pair n)
+  (map make-pair-sum
+       (filter prime-sum-pair?
+	       (uniq-pairs n))))
+
+(prime-sum-pair 20)
