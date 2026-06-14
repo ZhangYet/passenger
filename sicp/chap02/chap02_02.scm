bc7cfaf91dd16923b1f331ed459b588004e26938
Dantezy <zhangyet@gmail.com>
2026-06-14 11:00:20 +0800

    Done 2.37

--- sicp/chap02/chap02_02.scm
+++ sicp/chap02/chap02_02.scm
@@ -167,4 +167,11 @@
 			     1
 			     (count-levevs-a elem))) x)))
 
-;;
+;; exec 2.37
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map car seqs))
+	    (accumulate-n op init (map cdr seqs)))))
+
+(accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9)))
