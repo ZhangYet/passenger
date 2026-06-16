711693d42caba263f6df10c3ffd8c732f1106e4c
Dantezy <zhangyet@gmail.com>
2026-06-16 16:18:04 +0800

    Done 2.60
    Signed-off-by: Dantezy <zhangyet@gmail.com>

--- sicp/chap02/chap02_03.scm
+++ sicp/chap02/chap02_03.scm
@@ -187,3 +187,27 @@
 	(else (error "unknown expression type -- DERIV" expr))))
 
 (define sample '(x * (x * y)))
+
+;; sec 2.3.3
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+	((equal? x (car set)) #t)
+	(else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+	((element-of-set? (car set1) set2)
+	 (cons (car set1) (intersection-set (cdr set1) set2)))
+	(else (intersection-set (cdr set1) set2))))
+
+;; exec 2.59
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+	((element-of-set? (car set1) set2)
+	 (union-set (cdr set1) set2))
+	(else (cons (car set1) (union-set (cdr set1) set2)))))
