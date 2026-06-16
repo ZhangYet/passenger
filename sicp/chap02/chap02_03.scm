c62a5f05d5c0397545dd95eae223187f8e82d8c4
Dantezy <zhangyet@gmail.com>
2026-06-17 00:23:23 +0800

    Done 2.62
    Signed-off-by: Dantezy <zhangyet@gmail.com>

--- sicp/chap02/chap02_03.scm
+++ sicp/chap02/chap02_03.scm
@@ -211,3 +211,41 @@
 	((element-of-set? (car set1) set2)
 	 (union-set (cdr set1) set2))
 	(else (cons (car set1) (union-set (cdr set1) set2)))))
+
+;; section 2.3.3
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+	((= x (car set)) #t)
+	((< x (car set)) #f)
+	(else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2)) '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+	(cond ((= x1 x2)
+	       (cons x1 (intersection-set (cdr set1) (cdr set2))))
+	      ((< x1 x2) (intersection-set (cdr set1) set2))
+	      ((< x2 x1) (intersection-set set1 (cdr set2)))))))
+
+;; exec 2.61
+(define (adjoin-set x set)
+  (if (null? set)
+      (cons x '())
+      (cond ((= x (car set)) set)
+	    ((< x (car set))
+	     (cons x set))
+	    ((> x (car set))
+	     (cons (car set) (adjoin-set x (cdr set)))))))
+
+(adjoin-set 1 (list 1 2 3 4 5))
+(adjoin-set 2 (list 1 3 4 5 6))
+(adjoin-set 6 (list 1 2 3 4 5))
+
+;; exec 2.62
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+	((null? set2) set1)
+	(else (let ((x1 (car set1)) (x2 (car set2)))
+		(cond ((= x1 x2) (cons x1 (union-set (cdr set1) (cdr set2))))
+		      ((> x1 x2) (cons x2 (union-set set1 (cdr set2))))
+		      ((< x1 x2) (cons x1 (union-set (cdr set1) set2))))))))
