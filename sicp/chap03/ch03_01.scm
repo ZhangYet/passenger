5f30611dceb3348a06a1b1d577951300a775a0c2
Dantezy <zhangyet@gmail.com>
2026-06-24 11:14:13 +0800

    Done 3.2
    Signed-off-by: Dantezy <zhangyet@gmail.com>

--- sicp/chap03/ch03_01.scm
+++ sicp/chap03/ch03_01.scm
@@ -6,3 +6,17 @@
 
 (define A1 (make-accumulator 10))
 (define A2 (make-accumulator 2))
+
+;; exec 3.2
+;; 参考答案和这个答案一样，处理不了多个参数的函数
+(define (make-monitor func)
+  (let ((count 0))
+    (define (dispatch m)
+      (cond ((eq? m 'how-many-times) count)
+	    ((eq? m 'reset-count) (begin (set! count 0)
+					 count))
+	    (else (begin (set! count (+ count 1))
+			 (func m)))))
+    dispatch))
+
+;;
