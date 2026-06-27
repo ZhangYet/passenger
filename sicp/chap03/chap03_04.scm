2d341b4d0fa4ddc6ab0623921b1fdf8870041dac
Dantezy <zhangyet@gmail.com>
2026-06-27 15:17:08 +0800

    Done 3.13
    Signed-off-by: Dantezy <zhangyet@gmail.com>

--- sicp/chap03/chap03_04.scm
+++ sicp/chap03/chap03_04.scm
@@ -7,3 +7,16 @@ z
 (define w (append! x y))
 w
 (cdr x)
+
+;; exec 3.13
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(define z (make-cycle (list 'a 'b 'c)))
+;; WARNING: (last-pair z) infinite loop
